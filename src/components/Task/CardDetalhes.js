a917fec3b4fe01267bfbb348f261045c8aa5cad0
BRENO PEREIRA ALVES <breno.alves@academico.ifrn.edu.br>
2020-03-09 15:57:29 -0300

    batata

--- src/components/Task/CardDetalhes.js
+++ src/components/Task/CardDetalhes.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-import '../Cards/Cards.sass'
+import '../Cards/Cards.sass';
 
 function CardDetalhes() {
     return (
         <>
             <div id="card-detalhes">
-
+                <div className="cabecalho-card col-lg-12">
+                    <h2 className="title">DETALHES</h2>
+                    <div>
+                        <button className="btn btn-outline-primary">
+                            EDITAR
+                        </button>
+                    </div>
+                </div>
             </div>
         </>
     );
