22080d778c9ad365043e9b4c5eaef652764a8097
jonathanccardoso <jonathan99moura@hotmail.com>
2020-03-10 18:55:27 -0300

    Adds styles and customs modal form

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import api from '../../services/api';
-import Modal from '../../components/Modal/Modal';
-import './Home.sass';
+import api from "../../services/api";
+import Modal from "../../components/Modal/Modal";
+import "./Home.sass";
 
 function Home() {
   const [show, setShow] = useState(true);
-
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   // const [subject, setSubject] = useState('');
-  const [due_date, setDueDate] = useState('');
-  const [description, setDescription] = useState('');
+  const [due_date, setDueDate] = useState("");
+  const [description, setDescription] = useState("");
   // const [to_do_list, setToDoList] = useState('');
-  const [user_id, setUserId] = useState('');
+  const [user_id, setUserId] = useState("");
 
-  useEffect(()=>{
+  const models = {
+
+  }
+
+  useEffect(() => {
     async function authUser() {
-      // const response = await axios
-      //   .post('https://gametask.com.br/user/auth', {
       const response = await api
-        .post('/user/auth', {
-          email: 'euller@gametask.com',
-          password: 'test123'
+        .post("/user/auth", {
+          email: "euller@gametask.com",
+          password: "test123"
         })
         .then(function(res) {
-          console.log(res, 'deu ruim!');
+          console.log(res, "deu ruim!");
         });
       console.log(response);
     }
@@ -34,33 +35,33 @@ function Home() {
 
   function showModal(e) {
     setShow(!show);
-  };
+  }
 
   async function handleCreateTask(e) {
     e.preventDefault();
 
     console.log(name);
-    
+
     const response = await api
-      .post('/task', {
+      .post("/task", {
         name,
         // subject,
         due_date,
         description,
-        // to_do_list,
+        to_do_list: [],
         user_id
       })
       .then(function(res) {
-        console.log(res, 'deu ruim!!!');
+        console.log(res, "deu ruim!!!");
       });
     console.log(response);
 
     setShow(!show);
-    setName('');
-    setDueDate('');
-    setDescription('');
-    setToDoList('');
-  };
+    setName("");
+    setDueDate("");
+    setDescription("");
+    // setToDoList('');
+  }
 
   return (
     <div className="box">
@@ -91,12 +92,24 @@ function Home() {
       </a>
 
       <Modal onClose={showModal} show={show}>
-
         <div className="models">
           <section className="container">
             <h2>Modelos</h2>
+            
+            {/* // <DevItem key={dev._id} dev={dev} /> */}
+            {devs.map(dev => (
+              <button
+                className="item activate"
+                onClick={e => {
+                  // showModal(e);
+                }}
+              >
+                <span>Disciplina</span>
+              </button>
+            ))}
+            
             <button
-              className="item"
+              className="item activate"
               onClick={e => {
                 // showModal(e);
               }}
@@ -122,17 +135,25 @@ function Home() {
           </section>
         </div>
 
-
         <div className="content">
           <section className="container">
-            <form onSubmit={handleCreateTask}>
-            
+            <form onSubmit={handleCreateTask} className="container_between">
               <h2>Cadastrar Atividade</h2>
+              
+              <div>
               <label htmlFor="name">
                 Nome
                 <div className="">
                   {/* <img src="/static/icons/form_icon/icon_task.png" className="input_icon" /> */}
-                  <input type="text" name="name" id="name" placeholder="Nome" value={name} required />
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    required
+                  />
                 </div>
               </label>
 
@@ -141,8 +162,16 @@ function Home() {
                   Disciplina
                   <div>
                     {/* <img src="/static/icons/form_icon/icon_subject.png" className="input_icon" /> */}
-                    <select className="select" name="subject" id="subject" value={subject} required>
-                      <option value="0" selected disabled> Escolha uma disciplina</option>
+                    <select
+                      className="select"
+                      name="subject"
+                      id="subject"
+                      required
+                    >
+                      <option value="0" selected disabled>
+                        {" "}
+                        Escolha uma disciplina
+                      </option>
                       <option value="1"> Disciplina 1</option>
                       <option value="2"> Disciplina 2</option>
                     </select>
@@ -152,7 +181,15 @@ function Home() {
                   Prazo
                   <div>
                     {/* <img src="/static/icons/form_icon/icon_date.png" className="input_icon" /> */}
-                    <input type="date" name="due_date" id="due_date" placeholder="dd/mm/aaaa" value={due_date} required />
+                    <input
+                      type="date"
+                      name="due_date"
+                      id="due_date"
+                      placeholder="dd/mm/aaaa"
+                      value={due_date}
+                      onChange={e => setDueDate(e.target.value)}
+                      required
+                    />
                   </div>
                 </label>
               </div>
@@ -163,9 +200,19 @@ function Home() {
                   {/* <img src="/static/icons/form_icon/icon_description.png" className="input_icon" /> */}
                   {/* <input type="text" name="description" id="description_field" placeholder="Descrição" className="input_text input_style font_description" required /> */}
 
-                  <textarea id="text" name="description" id="description" value={description} rows="5" placeholder="Descrição" />
+                  <textarea
+                    id="text"
+                    name="description"
+                    id="description"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                    rows="5"
+                    placeholder="Descrição"
+                  />
                 </div>
               </label>
+              </div>
+
 
               <div className="between">
                 <button
@@ -175,15 +222,16 @@ function Home() {
                     showModal(e);
                   }}
                 >
-                  <span><i className="fa fa-times" aria-hidden="true"></i>Cancelar</span>
+                  <span>
+                    <i className="fa fa-times" aria-hidden="true"></i>Cancelar
+                  </span>
                 </button>
-                <button
-                  type="submit"
-                >
-                  <span><i className="fa fa-times" aria-hidden="true"></i>Confirmar</span>
+                <button type="submit">
+                  <span>
+                    <i className="fa fa-times" aria-hidden="true"></i>Confirmar
+                  </span>
                 </button>
               </div>
-
             </form>
           </section>
         </div>
@@ -193,21 +241,36 @@ function Home() {
             <h2>Lista de Afarezes</h2>
 
             <label htmlFor="to_do_list">
-              <div id='to_do_list'>
+              <div id="to_do_list">
                 <div className="flex">
                   <span id="btn_todo">
-                    <span className="btn_closed"><i className="fa fa-times" aria-hidden="true"></i></span>
+                    <span className="btn_closed">
+                      <i className="fa fa-times" aria-hidden="true"></i>
+                    </span>
                   </span>
                   <div className="">
                     <div>
                       {/* <img src="/static/icons/form_icon/icon_todo.png" className="input_icon" /> */}
                       {/* <span><i class="fa fa-times" aria-hidden="true"></i></span> */}
-                      <input type="text" name="to_do_name_0" id="to_do_name_field" placeholder="Questão 1" className="input_text input_style font_description" required />
+                      <input
+                        type="text"
+                        name="to_do_name_0"
+                        id="to_do_name_field"
+                        placeholder="Questão 1"
+                        className="input_text input_style font_description"
+                        required
+                      />
                     </div>
                     <div>
                       {/* <img src="/static/icons/form_icon/icon_description.png" className="input_icon" /> */}
                       {/* <span><i class="fa fa-times" aria-hidden="true"></i></span> */}
-                      <textarea id="to_do_description_field" name="to_do_description_0" rows="2" placeholder="Descrição" required />
+                      <textarea
+                        id="to_do_description_field"
+                        name="to_do_description_0"
+                        rows="2"
+                        placeholder="Descrição"
+                        required
+                      />
                     </div>
                   </div>
                 </div>
@@ -215,12 +278,9 @@ function Home() {
             </label>
 
             {/* <span><i className="fa fa-times" aria-hidden="true"></i></span> */}
-
           </section>
         </div>
-
       </Modal>
-
     </div>
   );
 }
