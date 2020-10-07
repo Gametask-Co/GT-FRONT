6d958d672ed69ea7ea880ee6c632f6dc4667754d
jonathanccardoso <jonathan99moura@hotmail.com>
2020-10-07 15:44:25 -0300

    Chore: adds component old modal

---
+++ src/components/Modal/styled.js
@@ -0,0 +1,156 @@
+// .modal {
+//   width: 74.21vw;
+//   height: 80vh;
+//   border: solid 1px rgba(255, 255, 255, 0.87);
+//   background: #121212 0% 0% no-repeat padding-box;
+//   opacity: 1;
+//   position: absolute;
+//   display: grid;
+// }
+
+// .models {
+//   grid-area: models;
+// }
+
+// .content {
+//   grid-area: content;
+//   top: 80px;
+//   background: #FFFFFF0D 0% 0% no-repeat padding-box;
+//   opacity: 1;
+// }
+
+// .todo {
+//   grid-area: todo;
+// }
+
+// .structure {
+//   grid-template-columns: 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw 2.18vw;
+//   column-gap: 1.25vw;
+//   grid-template-areas: "models models models models models content content content content content content content content content todo todo todo todo todo todo todo todo";
+// }
+
+// .container {
+//   display: flex;
+//   margin: 26px 11px 0 44px;
+//   align-items: stretch;
+//   flex-direction: column;
+// }
+
+// .item {
+//   width: 160px;
+//   height: 48px;
+//   box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+//   background-color: rgba(255, 255, 255, 0.07);
+//   margin-top: 20px;
+// }
+
+// .item:first-of-type {
+//   margin-top: 40px;
+// }
+
+// .between {
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: flex-end;
+// }
+
+// .label_margin {
+//   margin-top: 40px;
+// }
+
+// .form_between {
+//   display: flex;
+//   justify-content: space-between;
+// }
+
+// .form_between label {
+//   width: 45%;
+// }
+
+// .flex {
+//   display: flex;
+// }
+
+// .btn_closed {
+//   margin: 18px;
+//   float: left;
+// }
+
+// .box {
+//   margin: 3vh 18.75vw;
+//   padding: 4vh auto;
+//   height: 92vh;
+//   width: 62.5vw;
+//   background-color: rgba(255, 255, 255, 0.02);
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+//   align-items: center;
+// }
+
+// #logotext {
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+//   align-items: center;
+// }
+
+// .item.activate {
+//   border: solid 1px #ffffff;
+//   background-color: rgba(255, 255, 255, 0.09);
+// }
+
+// .container_between {
+//   height: 25vw;
+//   display: flex;
+//   justify-content: space-between;
+//   flex-direction: column;
+// }
+
+// .modal_message {
+//   width: 336px;
+//   height: 129px;
+//   background-color: #121212;
+//   position: absolute;
+//   bottom: 0;
+//   right: 0;
+//   margin: 20px;
+//   color: white;
+// }
+
+// .modal_message .margin_center {
+//   margin: 2vw;
+// }
+
+// .alert_block {
+//   width: 33.125vw;
+//   height: 32.5vh;
+//   border: solid 1px rgba(255, 255, 255, 0.87);
+//   background: #121212 0% 0% no-repeat padding-box;
+//   opacity: 1;
+//   position: absolute;
+//   display: flex;
+//   flex-direction: column;
+//   color: white;
+//   padding: 4px 40px;
+// }
+
+// .alert_block h1 {
+//   font-size: 2rem;
+//   line-height: 6rem;
+// }
+
+// .alert_block p {
+//   font-style: italic;
+// }
+
+// .check {
+//   display: flex;
+//   flex-direction: row;
+//   margin: 13px 0px;
+// }
+
+// .check input {
+//   width: 0;
+//   margin-right: 10px;
+// }
