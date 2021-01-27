fd800fae42e956c6bf1636dede254b1b5e06007f
jonathanccardoso <jonathan99moura@hotmail.com>
2021-01-27 17:56:17 -0300

    :sparkles: Fixme adds page 404 and in router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from "../../pages/Home";
-import Dashboard from "../../pages/Dashboard";
 import SignIn from "../../pages/SignIn";
 import SignUp from "../../pages/SignUp";
 import Subject from "../../pages/Subject";
 import SubjectDetail from "../../pages/SubjectDetail";
 import MilestoneDetail from "../../pages/MilestoneDetail";
+import Page404 from "../../pages/Page404";
 
 import AuthProvider from "../../contents/auth";
 
@@ -16,13 +15,13 @@ function App() {
     <AuthProvider>
       <Router>
         <Switch>
-          {/* <Route exact path="/" component={Home} /> */}
-          {/* <Route path="/dashboard" component={Dashboard} /> */}
           <Route path="/signin" component={SignIn} />
           <Route path="/signup" component={SignUp} />
           <Route exact path="/" component={Subject} />
           <Route path="/subject/:id" component={SubjectDetail} />
           <Route path="/milestone/:id" component={MilestoneDetail} />
+
+          <Route path="*" component={Page404} status={404} />
         </Switch>
       </Router>
     </AuthProvider>
