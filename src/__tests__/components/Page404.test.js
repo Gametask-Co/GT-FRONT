8239c6789ba80dfe8b5972abcc3e4232603b3dc2
jonathanccardoso <jonathan99moura@hotmail.com>
2021-02-22 21:55:05 -0300

    :white_check_mark: Fixme: update struture test and rename test

--- src/__tests__/components/Page404.test.js
+++ src/__tests__/components/Page404.test.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { act } from "@testing-library/react-hooks";
-
-import { render, screen } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
+
+import { act } from "@testing-library/react-hooks";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import Page404 from "../../pages/Page404";
 
-describe("Test routes on App component", () => {
+describe("Test routes on Page404 component", () => {
   it("Should check bad router", () => {
     const history = createMemoryHistory();
     history.push("/bad/request");
--- src/__tests__/components/Subject.test.js
+++ src/__tests__/components/Subject.test.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 import { act } from "@testing-library/react-hooks";
-import { render, fireEvent, cleanup, waitFor } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitFor,
+  screen,
+} from "@testing-library/react";
 
 import App from "../../components/App";
 import Subject from "../../pages/Subject";
+import CardSubjectList from "../../components/CardSubjectList";
 
 beforeAll(() => {
   jest.fn().mockResolvedValue({
@@ -20,48 +27,38 @@ describe("Testing integration a Subject component", () => {
   it("make sure I can submit on modal and show data on screen", async () => {
     jest.mock("axios");
 
-    jest.fn().mockResolvedValue({
-      data: [
-        { id: 1, name: "Algoritmos", description: "Disciplina do 2periodo" },
-      ],
+    act(() => {
+      render(
+        <App>
+          <Subject>
+            <CardSubjectList
+              name="Algoritmos"
+              description="Disciplina do 2periodo"
+            />
+          </Subject>
+        </App>
+      );
     });
 
-    const mockFetch = jest.fn(() => Promise.resolve());
-
-    const { getByPlaceholderText, getByTestId, getByText, debug } = render(
-      <App>
-        <Subject
-          subjects={[
-            {
-              id: 1,
-              name: "Algoritmos",
-              description: "Disciplina do 2periodo",
-            },
-          ]}
-        />
-      </App>
-    );
-
-    act(async () => {
-      const openModalButton = getByTestId("open-modal-button");
+    act(() => {
+      const openModalButton = screen.getByTestId("open-modal-button");
       fireEvent.click(openModalButton);
     });
 
-    const cardSubjectList = getByTestId("card-subject");
-    const nameInput = getByPlaceholderText("Nome da Disciplina");
-    const descriptionInput = getByPlaceholderText("Escreva aqui...");
-    const submitButton = getByTestId("submit-create-subject-button");
+    const cardSubjectList = screen.getByTestId("card-subject");
+    const nameInput = screen.getByPlaceholderText("Nome da Disciplina");
+    const descriptionInput = screen.getByPlaceholderText("Escreva aqui...");
+    const submitButton = screen.getByTestId("submit-create-subject-button");
 
-    act(async () => {
+    act(() => {
       fireEvent.change(nameInput, { target: { value: "Algoritmos" } });
       fireEvent.change(descriptionInput, {
         target: { value: "Disciplina do 2periodo" },
       });
       fireEvent.click(submitButton);
-      fireEvent.click(getByText(/concluir/i));
+      fireEvent.click(screen.getByText(/concluir/i));
     });
 
-    waitFor(async () => expect(getByText("Algoritmos")).toBeTruthy());
-    // debug();
+    waitFor(() => expect(screen.getByText("Algoritmos")).toBeTruthy());
   });
 });
