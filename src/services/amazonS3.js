934f4a64fe8c3e4317a1d3e5c629ebd4e5fe36d5
Breno Alves <breno550@hotmail.com>
2021-03-20 16:59:30 -0300

    changing envs on amazonS3 file

--- src/services/amazonS3.js
+++ src/services/amazonS3.js
@@ -1,11 +1,11 @@
 import { uploadFile, deleteFile } from "react-s3";
 
 const config = {
-  bucketName: process.env.S3_BUCKET_NAME,
-  dirName: process.env.S3_DIR_NAME,
-  region: process.env.S3_REGION,
-  accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  bucketName: process.env.REACT_APP_S3_BUCKET_NAME,
+  dirName: process.env.REACT_APP_S3_DIR_NAME,
+  region: process.env.REACT_APP_S3_REGION,
+  accessKeyId: process.env.REACT_APP_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_S3_SECRET_ACCESS_KEY,
 };
 
 export const UploadFile = (file) => uploadFile(file, config);
