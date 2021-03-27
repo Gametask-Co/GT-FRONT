5cc3f7769dfe688b63998b0dca44174c418cd2d3
jonathanccardoso <jonathan99moura@hotmail.com>
2021-03-27 17:49:48 -0300

    Fixme: finish header title on pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useAuth } from '../../contents/auth';
-import { Link, useHistory } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { useAuth } from "../../contents/auth";
+import { Link, useHistory } from "react-router-dom";
 
-import * as Styled from './styled';
-import { DEFAULT_THEME as theme } from '../../styles/constants';
+import * as Styled from "./styled";
+import { DEFAULT_THEME as theme } from "../../styles/constants";
 
-import DropdownMenu from '../DropdownMenu';
-import { GametaskLG, MessageMD, BellMD } from '../Icons';
-import { AvatarLG } from '../Avatar';
+import DropdownMenu from "../DropdownMenu";
+import { GametaskLG, MessageMD, BellMD } from "../Icons";
+import { AvatarLG } from "../Avatar";
 
 const Header = ({ Title }) => {
   const { signOut } = useAuth();
   const history = useHistory();
-
-  const pageBack = () => {
-    history.goBack();
-  };
 
   return (
     <Styled.Header>
       <Styled.LeftSide>
         <GametaskLG right={theme.spacing.lg} />
         {Title && (
-          <Styled.WrapperTitle to={pageBack}>
+          // <Styled.WrapperTitle onClick={() => pageBack}>
+          <Styled.WrapperTitle
+            onClick={() => {
+              history.push("/");
+            }}
+          >
             <Styled.ArrowLeftIcon />
             <Styled.Title>{Title}</Styled.Title>
           </Styled.WrapperTitle>
